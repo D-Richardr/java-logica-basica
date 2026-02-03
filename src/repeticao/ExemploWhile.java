1792bfc8ecacbdf29014a26a9338c89ad92af44f
Daniel Richard <156822786+D-Richardr@users.noreply.github.com>
2026-02-03 20:54:59 -0300

    Create ExemploWhile.java

---
+++ src/repeticao/ExemploWhile.java
@@ -0,0 +1,14 @@
+package repeticao;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+
+    }
+}
