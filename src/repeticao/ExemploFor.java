40ec0bed13a939e55b098d9ca4f9140b60b1546a
Daniel Richard <156822786+D-Richardr@users.noreply.github.com>
2026-02-03 20:53:55 -0300

    Create ExemploFor.java

---
+++ src/repeticao/ExemploFor.java
@@ -0,0 +1,11 @@
+package repeticao;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Número: " + i);
+        }
+
+    }
+}
